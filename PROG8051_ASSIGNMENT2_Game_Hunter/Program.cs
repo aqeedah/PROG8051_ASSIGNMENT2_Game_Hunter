56947cb71c09a0883841b23b484541d8b294995e
Aqeedah <aqeedah2594@gmail.com>
2024-02-17 15:19:16 -0500

    Add project files.

---
+++ PROG8051_ASSIGNMENT2_Game_Hunter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static string[,] Board = { { "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-" }, { "-", "-", "-", "-", "-", "-" } };
+        static int totalTurn = 0;
+        static int[] p1Position = new int[] { 0, 0 };
+        static int[] p2Position = new int[] { 5, 5 };
+        static int p1Gemcount = 0;
+        static int p2Gemcount = 0;
+
+        static void displayBoard(string[,] Board)
+        {
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                for(int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Console.Write(Board[i, j] + "  ");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            displayBoard(Board);
+        }
+    }
+}
